99a885f0dc062ce8211645a3677983794f7d6978
Leonardo Perin <155500125+Perinzinho@users.noreply.github.com>
2026-06-02 15:56:12 -0300

    WIP-UserEntity

---
+++ BackEnd/src/Domain/Entities/UserEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace DEMP_RPG_API.Domain.Entities;
+
+public class UserEntity
+{
+    public Guid Id { get; private set; }
+    public string Username { get; private set; }
+    public string PasswordHash { get; private set; }
+    
+}
