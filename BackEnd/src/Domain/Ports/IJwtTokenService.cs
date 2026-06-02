07c4017902e321697d711a9c5b962f0dbd6a33f1
Leonardo Perin <155500125+Perinzinho@users.noreply.github.com>
2026-06-02 16:05:54 -0300

    WIP:JWTTokenService implementation

---
+++ BackEnd/src/Domain/Ports/IJwtTokenService.cs
@@ -0,0 +1,8 @@
+﻿using DEMP_RPG_API.Domain.Entities;
+
+namespace DEMP_RPG_API.Domain.Ports;
+
+public interface IJwtTokenService
+{
+    public string GenerateToken(UserEntity User);
+}
---
+++ BackEnd/src/Infrastructure/JwtTokenService.cs
@@ -0,0 +1,19 @@
+﻿using DEMP_RPG_API.Domain.Entities;
+using DEMP_RPG_API.Domain.Ports;
+
+namespace DEMP_RPG_API.Infrastructure;
+
+public class JwtTokenService
+{
+    private readonly IJwtTokenService _jwtTokenService;
+
+    public LoginUseCase(IJwtTokenService jwtTokenService)
+    {
+        _jwtTokenService = jwtTokenService;
+    }
+
+    public string Execute(UserEntity user)
+    {
+        return _jwtTokenService.GenerateToken(user);
+    }
+}
