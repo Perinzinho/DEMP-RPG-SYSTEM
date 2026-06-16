36fb3f152c2c604748177317f03a9b8f851f041c
Leonardo Perin <155500125+Perinzinho@users.noreply.github.com>
2026-06-16 20:45:41 -0300

    feat: Character Entity and Ocuppation Enum

---
+++ BackEnd/src/Domain/Entities/CharacterEntity.cs
@@ -0,0 +1,36 @@
+﻿using DEMP_RPG_API.Domain.Enums;
+
+namespace DEMP_RPG_API.Domain.Entities;
+
+public class CharacterEntity
+{
+    public Guid Id { get; private set; }
+    public Guid UserId { get; private set; }
+    public Guid RoomId { get; private set; }
+    public string Name {get; private set;}
+    public string Gender {get; private set;}
+    public OccupationsEnum Occupation {get; private set;}//Note: See if it's possible to minimize occupation Enum
+    public string Residence {get; private set;}
+    public int Age {get; private set;}//ToDo-Validation for Age
+    public string Annotations{get; private set;}
+    public List<Guid> ItemIds{get; private set;}
+    public DateTime CreatedAt {get; private set;}
+    public DateTime? UpdatedAt {get; private set;}
+
+    public CharacterEntity(Guid id, Guid userId, Guid roomId, string name, string gender, OccupationsEnum occupation,
+        string residence, int age, string annotations, List<Guid> itemIds)
+    {
+        Id = id;
+        UserId = userId;
+        RoomId = roomId;
+        Name = name;
+        Gender = gender;
+        Occupation = occupation;
+        Residence = residence;
+        Age = age;
+        Annotations = annotations;
+        ItemIds = itemIds;
+        CreatedAt = DateTime.Now;
+        UpdatedAt = null;
+    }
+}
