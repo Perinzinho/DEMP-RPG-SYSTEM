22264293b9dfb452e8540197736055b71b669fc0
Leonardo Perin <155500125+Perinzinho@users.noreply.github.com>
2026-06-16 14:38:48 -0300

    feat: Implemented IUserRepository

--- BackEnd/src/Infrastructure/Repositories/UserRepository.cs
+++ BackEnd/src/Infrastructure/Repositories/UserRepository.cs
@@ -1,6 +1,40 @@
-﻿namespace DEMP_RPG_API.Infrastructure.Repositories;
+﻿using DEMP_RPG_API.Domain.Entities;
+using DEMP_RPG_API.Domain.Ports;
+using Microsoft.EntityFrameworkCore;
 
-public class UserRepository
+namespace DEMP_RPG_API.Infrastructure.Repositories;
+
+public class UserRepository: IUserRepository
 {
-    //ToDo-Implementar Repository
+    private readonly AppDbContext _context;
+
+    public UserRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<UserEntity> Create(UserEntity user)
+    {
+        await _context.Users.AddAsync(user);
+        await _context.SaveChangesAsync();
+        return user;
+    }
+
+    public async Task<UserEntity?> GetUserByEmail(string email)
+    {
+        var result = await _context.Users.FirstOrDefaultAsync(e=>e.Email == email);
+        return result;
+    }
+
+    public async Task<UserEntity?> GetUserById(Guid id)
+    {
+        var result = await _context.Users.FirstOrDefaultAsync(e => e.Id == id);
+        return result;
+    }
+
+    public async Task<IEnumerable<UserEntity?>> GetAllUsers()
+    {
+        var result = await _context.Users.ToListAsync();
+        return result;
+    }
 }
