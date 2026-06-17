1254ea1bd11852bbe2613986574ed8c9bd489fcf
Leonardo Perin <155500125+Perinzinho@users.noreply.github.com>
2026-06-17 20:47:12 -0300

    feat: Created RoomEntity

---
+++ BackEnd/src/Domain/Entities/RoomEntity.cs
@@ -0,0 +1,36 @@
+﻿using DEMP_RPG_API.Domain.Enums;
+
+namespace DEMP_RPG_API.Domain.Entities;
+
+public class RoomEntity
+{
+    public Guid Id { get; private set; }
+    public Guid MasterId { get; private set; }
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public List<Guid> UserIds { get; private set; }
+    public SheetEnum SheetEnum { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime? UpdatedAt { get; private set; }
+    
+    
+    public RoomEntity(Guid id, Guid masterId, string name, string description, List<Guid>usersIds,SheetEnum sheetEnum, DateTime createdAt, DateTime? updatedAt)
+    {
+        Id = id;
+        MasterId = masterId;
+        Name = name;
+        Description = description;
+        UserIds = usersIds;
+        SheetEnum = sheetEnum;
+        CreatedAt = DateTime.UtcNow;
+        UpdatedAt = null;
+    }
+
+    public void Update(string name, string description, SheetEnum sheetEnum)
+    {
+        if (Name != name) Name = name;
+        if(Description != description) Description = description;
+        if(SheetEnum != sheetEnum) SheetEnum = sheetEnum;
+        UpdatedAt = DateTime.UtcNow;
+    }
+}
---
+++ BackEnd/src/Domain/Enums/SheetEnum.cs
@@ -0,0 +1,7 @@
+﻿namespace DEMP_RPG_API.Domain.Enums;
+
+public enum SheetEnum
+{
+    Modern=1,
+    Old=2
+}
