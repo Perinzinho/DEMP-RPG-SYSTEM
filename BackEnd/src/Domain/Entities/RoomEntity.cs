f7de74a2dc6feec4e5a0b157dea8fde229f9d810
Leonardo Perin <155500125+Perinzinho@users.noreply.github.com>
2026-06-18 10:36:59 -0300

    feat: created Simple GenerateRoomCode function

--- BackEnd/src/Domain/Entities/RoomEntity.cs
+++ BackEnd/src/Domain/Entities/RoomEntity.cs
@@ -4,7 +4,9 @@ namespace DEMP_RPG_API.Domain.Entities;
 
 public class RoomEntity
 {
+    //ToDo-Implementation of optional rules
     public Guid Id { get; private set; }
+    public string RoomCode { get; private set; }
     public Guid MasterId { get; private set; }
     public string Name { get; private set; }
     public string Description { get; private set; }
@@ -14,9 +16,10 @@ public class RoomEntity
     public DateTime? UpdatedAt { get; private set; }
     
     
-    public RoomEntity(Guid id, Guid masterId, string name, string description, List<Guid>usersIds,SheetEnum sheetEnum, DateTime createdAt, DateTime? updatedAt)
+    public RoomEntity(Guid id,string roomCode, Guid masterId, string name, string description, List<Guid>usersIds,SheetEnum sheetEnum, DateTime createdAt, DateTime? updatedAt)
     {
         Id = id;
+        RoomCode = roomCode;
         MasterId = masterId;
         Name = name;
         Description = description;
@@ -32,5 +35,12 @@ public class RoomEntity
         if(Description != description) Description = description;
         if(SheetEnum != sheetEnum) SheetEnum = sheetEnum;
         UpdatedAt = DateTime.UtcNow;
+    }
+
+    private static string GenerateRoomCode()//Simple way to generate room code, could be changed later for something better
+    {
+        //ToDo-Make Generate RoomCode more impredictable
+        Random random = new Random();
+        return random.Next(100000,100000).ToString();
     }
 }
---
+++ BackEnd/src/Domain/Ports/IRoomRepository.cs
@@ -0,0 +1,8 @@
+﻿using DEMP_RPG_API.Domain.Entities;
+
+namespace DEMP_RPG_API.Domain.Ports;
+
+public interface IRoomRepository
+{
+    Task<RoomEntity> CreateRoom(RoomEntity room);
+}
