b57340e6a20dcf0489dc1e08e2c98df0b2fffc9e
Leonardo Perin <155500125+Perinzinho@users.noreply.github.com>
2026-06-18 20:48:21 -0300

    feat: created Update, create request and Get response CharacterStats

---
+++ BackEnd/src/Application/DTOs/Request/CharacterStats/UpdateCharacterRequestDTO.cs
@@ -0,0 +1,28 @@
+﻿using DEMP_RPG_API.Domain.Enums;
+
+namespace DEMP_RPG_API.Application.DTOs.Request.CharacterStats;
+
+public record UpdateCharacterRequestDTO(
+    MaxAttributesEnum  MaxAttributes,
+    int Strength,
+    int Dexterity,
+    int Intelligence,
+    int Size,
+    int Power,
+    int Appearance,
+    int Education,
+    int HitPoints,
+    int CurrentHp,
+    int Luck,
+    int Sanity,
+    int CurrentSanity,
+    int Move,
+    int Build,
+    int Dodge,
+    DamageBonusEnum DamageBonus,
+    bool TemporaryInsanity,
+    bool IndefiniteSanity,
+    bool MajorWound,
+    bool Unconscious,
+    bool Dying
+);
---
+++ BackEnd/src/Application/DTOs/Response/CharacterStats/GetCharacterStatsResponseDTO.cs
@@ -0,0 +1,30 @@
+﻿using DEMP_RPG_API.Domain.Enums;
+
+namespace DEMP_RPG_API.Application.DTOs.Response.CharacterStats;
+
+public record GetCharacterStatsResponseDTO(
+    Guid Id,
+    Guid CharacterId,
+    MaxAttributesEnum  MaxAttributes,
+    int Strength,
+    int Dexterity,
+    int Intelligence,
+    int Size,
+    int Power,
+    int Appearance,
+    int Education,
+    int HitPoints,
+    int CurrentHp,
+    int Luck,
+    int Sanity,
+    int CurrentSanity,
+    int Move,
+    int Build,
+    int Dodge,
+    DamageBonusEnum DamageBonus,
+    bool TemporaryInsanity,
+    bool IndefiniteSanity,
+    bool MajorWound,
+    bool Unconscious,
+    bool Dying
+    );
