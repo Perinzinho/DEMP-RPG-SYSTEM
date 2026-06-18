c894957282fb8da4b845668b8fa00d4a91c5c966
Leonardo Perin <155500125+Perinzinho@users.noreply.github.com>
2026-06-18 20:53:04 -0300

    feat: Implemented CharacterStatsMapper

---
+++ BackEnd/src/Adapters/Mappers/CharacterStatsMapper.cs
@@ -0,0 +1,36 @@
+﻿using DEMP_RPG_API.Application.DTOs.Response.CharacterStats;
+using DEMP_RPG_API.Domain.Entities;
+
+namespace DEMP_RPG_API.Adapters.Mappers;
+
+public class CharacterStatsMapper
+{
+    public static GetCharacterStatsResponseDTO ToGetResponse(CharacterStatsEntity characterStats)
+    {
+        return new GetCharacterStatsResponseDTO(characterStats.Id,
+            characterStats.CharacterId,
+            characterStats.MaxAttributes,
+            characterStats.Strength,
+            characterStats.Dexterity,
+            characterStats.Intelligence,
+            characterStats.Size,
+            characterStats.Power,
+            characterStats.Appearance,
+            characterStats.Education,
+            characterStats.HitPoints,
+            characterStats.CurrentHp,
+            characterStats.Luck,
+            characterStats.Sanity,
+            characterStats.CurrentSanity,
+            characterStats.Move,
+            characterStats.Build,
+            characterStats.Dodge,
+            characterStats.DamageBonus,
+            characterStats.TemporaryInsanity,
+            characterStats.IndefiniteSanity,
+            characterStats.MajorWound,
+            characterStats.Unconscious,
+            characterStats.Dying
+            );
+    }
+}
