ea67640bf6c57b6645560712c4506c82414d089f
Leonardo Perin <155500125+Perinzinho@users.noreply.github.com>
2026-06-20 22:41:09 -0300

    fix: forget to add characterService

---
+++ FrontEnd/src/services/characterService.js
@@ -0,0 +1,13 @@
+import { apiFetch } from './api';
+
+export function getCharactersByUserId(userId) {
+  return apiFetch(`/characters/user/${userId}`, { method: 'GET' });
+}
+
+export function getCharacterById(id) {
+  return apiFetch(`/characters/${id}`, { method: 'GET' });
+}
+
+export function getCharactersByRoomId(roomId) {
+  return apiFetch(`/characters/room/${roomId}`, { method: 'GET' });
+}
