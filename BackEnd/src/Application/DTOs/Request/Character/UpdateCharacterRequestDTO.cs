8462f73ad9edde012b2006b74a7663e10757f553
Leonardo Perin <155500125+Perinzinho@users.noreply.github.com>
2026-06-22 14:10:19 -0300

    fix: update character requestDTO beign non nullable

--- BackEnd/src/Application/DTOs/Request/Character/UpdateCharacterRequestDTO.cs
+++ BackEnd/src/Application/DTOs/Request/Character/UpdateCharacterRequestDTO.cs
@@ -3,11 +3,11 @@
 namespace DEMP_RPG_API.Application.DTOs.Request.Character;
 
 public record UpdateCharacterRequestDTO(
-    string Name,
-    string Gender,
-    OccupationsEnum Occupation,
-    string Residence,
-    int Age,
-    string Annotations,
+    string? Name,
+    string? Gender,
+    OccupationsEnum? Occupation,
+    string? Residence,
+    int? Age,
+    string? Annotations,
     List<Guid>? ItemIds
     );
